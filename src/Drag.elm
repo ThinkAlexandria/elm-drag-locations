60063c376ab090e52fdf76c3d53a42c9dfda6b99
YetAnotherMinion <yam@thinkalexandria.com>
2017-06-01 21:14:27 -0400

    dump source code to open source

---
+++ src/Drag.elm
@@ -0,0 +1,151 @@
+module Drag
+    exposing
+        ( State
+        , Msg(Start, Moved, End, Click)
+        , onMouseDown
+        , subscriptions
+        , update
+        , init
+        , delta
+        )
+{-| Open sourced this snippet so I could hack on new idea on train laptop. TODO:
+fill in readme details.
+
+
+@docs State, Msg, onMouseDown, subscriptions, update, init, delta
+
+-}
+
+import VirtualDom
+import Mouse exposing (Position)
+import Json.Decode
+
+{-| -}
+type State interactionLocation
+    = MouseDown
+        { current : Position
+        , start : Position
+        , location : interactionLocation
+        }
+    | MouseMoved
+        { current : Position
+        , start : Position
+        , location : interactionLocation
+        }
+    | NotDragging
+
+
+{-| -}
+init : State interactionLocation
+init =
+    NotDragging
+
+
+{-| -}
+type Msg interactionLocation
+    = Start interactionLocation Position
+    | Moved interactionLocation Position
+    | End interactionLocation Position
+    | Click interactionLocation Position
+
+
+type alias Delta =
+    ( Int, Int )
+
+
+{-| -}
+update : Msg interactionLocation -> State interactionLocation -> State interactionLocation
+update msg model =
+    case msg of
+        Start location xy ->
+            MouseDown
+                { current = xy
+                , start = xy
+                , location = location
+                }
+
+        Moved location xy ->
+            case model of
+                NotDragging ->
+                    NotDragging
+
+                MouseDown { start } ->
+                    MouseMoved
+                        { current = xy
+                        , start = start
+                        , location = location
+                        }
+
+                MouseMoved { start } ->
+                    MouseMoved
+                        { current = xy
+                        , start = start
+                        , location = location
+                        }
+
+        End location xy ->
+            NotDragging
+
+        Click location xy ->
+            NotDragging
+
+
+{-| -}
+delta : Msg interactionLocation -> State interactionLocation -> ( Int, Int )
+delta msg model =
+    case msg of
+        Start location xy ->
+            ( 0, 0 )
+
+        Click _ _ ->
+            ( 0, 0 )
+
+        Moved location xy ->
+            case model of
+                NotDragging ->
+                    ( 0, 0 )
+
+                MouseDown { current } ->
+                    ( xy.x - current.x, xy.y - current.y )
+
+                MouseMoved { current } ->
+                    ( xy.x - current.x, xy.y - current.y )
+
+        End location xy ->
+            case model of
+                NotDragging ->
+                    ( 0, 0 )
+
+                MouseDown { current } ->
+                    ( xy.x - current.x, xy.y - current.y )
+
+                MouseMoved { current } ->
+                    ( xy.x - current.x, xy.y - current.y )
+
+
+{-| -}
+onMouseDown : VirtualDom.Options -> location -> VirtualDom.Property (Msg location)
+onMouseDown options interactionLocation =
+    VirtualDom.onWithOptions "mousedown"
+        options
+        (Json.Decode.map (\p -> Start interactionLocation p) Mouse.position)
+
+
+{-| -}
+subscriptions : State interactionLocation -> Sub (Msg interactionLocation)
+subscriptions lastState =
+    case lastState of
+        MouseDown { location } ->
+            Sub.batch
+                [ Mouse.moves (\p -> Moved location p)
+                , Mouse.ups (\p -> Click location p)
+                ]
+
+        MouseMoved { location } ->
+            Sub.batch
+                [ Mouse.moves (\p -> Moved location p)
+                , Mouse.ups (\p -> End location p)
+                ]
+
+        NotDragging ->
+            Sub.none
